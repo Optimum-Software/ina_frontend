fc492886a108d853c145c4e1ee008bb6d6f5352e
Bert de Boer <bertde.boer@hotmail.com>
2018-12-04 15:32:54 +0100

    basic login page

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -6,7 +6,8 @@ import {
   TextInput,
   TouchableOpacity,
   StyleSheet,
-  Image
+  Image,
+  ScrollView
 } from "react-native";
 
 import logo from "../assets/logo.png";
@@ -31,47 +32,43 @@ class LoginScreen extends Component {
 
   render() {
     return (
-      <View>
-        <View style={styles.container}>
-          <Image resizeMode="contain" style={styles.logo} source={logo} />
-          <View style={styles.inputContainer}>
-            <TextInput
-              style={styles.input}
-              keyboardType="email-address"
-              returnKeyType="next"
-              onChangeText={text => this.setState({ email: text })}
-              placeholder="E-mailadres"
-              placeholderTextColor="rgba(225,225,225,0.7)"
-            />
-            <TextInput
-              style={styles.input}
-              autoCapitalize="none"
-              onChangeText={text => this.setState({ password: text })}
-              secureTextEntry={true}
-              returnKeyType="go"
-              placeholder="Wachtwoord"
-              placeholderTextColor="rgba(225,225,225,0.7)"
-            />
-
-            <TouchableOpacity
-              style={styles.buttonContainer}
-              onPress={() => this.login()}
-            >
-              <Text style={styles.buttonText}>Inloggen</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={{ marginBottom: 25, marginTop: 10 }}
-              onPress={() =>
-                this.props.navigation.navigate("RegistrationScreen")
-              }
-            >
-              <Text style={{ color: "#fff" }}>
-                Nog geen account? Meld je aan!
-              </Text>
-            </TouchableOpacity>
-          </View>
+      <ScrollView style={styles.container}>
+        <Image resizeMode="contain" style={styles.logo} source={logo} />
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            keyboardType="email-address"
+            returnKeyType="next"
+            onChangeText={text => this.setState({ email: text })}
+            placeholder="E-mailadres"
+            placeholderTextColor="#37474f"
+          />
+          <TextInput
+            style={styles.input}
+            autoCapitalize="none"
+            onChangeText={text => this.setState({ password: text })}
+            secureTextEntry={true}
+            returnKeyType="go"
+            placeholder="Wachtwoord"
+            placeholderTextColor="#37474f"
+          />
         </View>
-      </View>
+
+        <TouchableOpacity
+          style={styles.buttonContainer}
+          onPress={() => this.login()}
+        >
+          <Text style={styles.buttonText}>Inloggen</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{ marginBottom: 25, marginTop: 10 }}
+          onPress={() => this.props.navigation.navigate("RegistrationScreen")}
+        >
+          <Text style={{ color: "#37474f" }}>
+            Nog geen account? Meld je aan!
+          </Text>
+        </TouchableOpacity>
+      </ScrollView>
     );
   }
 }
@@ -79,19 +76,20 @@ class LoginScreen extends Component {
 // define your styles
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     height: "100%",
     width: "100%",
     padding: 20,
-    backgroundColor: "black"
+    backgroundColor: "#fafafa"
   },
   inputContainer: {
-    marginTop: "30%"
+    marginTop: "25%"
   },
   input: {
     height: 40,
     marginBottom: 10,
     padding: 10,
-    color: "#fff"
+    backgroundColor: "#fff"
   },
   buttonContainer: {
     backgroundColor: "#2980b6",
@@ -105,8 +103,8 @@ const styles = StyleSheet.create({
   logo: {
     marginTop: "10%",
     alignSelf: "center",
-    height: "25%",
-    width: "25%"
+    height: 150,
+    width: 150
   }
 });
 export default LoginScreen;
