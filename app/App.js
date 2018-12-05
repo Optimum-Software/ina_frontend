217ef01044ccf3a4f26d28019a66f02c266dcdc1
Gaauwe <gromdroid@gmail.com>
2018-12-05 14:38:47 +0100

    little fix

--- app/App.js
+++ app/App.js
@@ -9,10 +9,6 @@ import React, { Component } from "react";
 import { Text, View } from "react-native";
 import { RootNavigator } from "./config/CreateNavigation";
 import firebaseApi from "./helpers/FirebaseApi";
-<<<<<<< HEAD
-=======
-
->>>>>>> upstream/master
 
 export default class App extends React.Component {
     constructor() {
