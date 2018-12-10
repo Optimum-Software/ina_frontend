9ff98740e4a97e231565d60c66307fc4b03d2165
unknown <jelmer.haarman@xs4all.nl>
2018-12-10 12:22:11 +0100

    started chatCollection screen

---
+++ app/screens/ChatCollection.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { TouchableOpacity, Text, View, FlatList, List } from "react-native";
+import { ListItem } from "react-native-elements";
+
+import firebaseApi from "../helpers/FirebaseApi";
+
+export default class ChatCollection extends Component {
+    constructor() {
+        super();
+        this.state = {
+            chats: [
+              {
+                key: 0,
+                title: "Gaauwe",
+                chatUserId: 1,
+              }
+            ]
+        };
+    }
+
+    componentDidMount() {
+      firebaseApi.login("mail@grombouts.nl", "123456")
+      //firebaseApi.getChats()
+    }
+
+    goToChat(uid) {
+      console.log(uid)
+    }
+
+    render() {
+        return (
+          <View>
+            <FlatList
+              data={this.state.chats}
+              renderItem={({ item, key }) => (
+                <View>
+                <ListItem
+                  key={key}
+                  title={item.title}
+                  chevron
+                  onPress={() => this.goToChat(item.title)}
+                />
+                </View>
+              )}
+            />
+          </View>
+        );
+    }
+}
