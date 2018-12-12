0c5bc78790511e3c9e773f1715e2821f17f2996d
Gaauwe <gromdroid@gmail.com>
2018-12-12 10:53:49 +0100

    Login layout

--- android/app/src/main/java/com/ina_frontend/MainApplication.java
+++ android/app/src/main/java/com/ina_frontend/MainApplication.java
@@ -3,6 +3,7 @@ package com.ina_frontend;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import io.invertase.firebase.RNFirebasePackage;
@@ -27,13 +28,9 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-            new MainReactPackage(),
-            new RNFirebasePackage(), 
-            new RNFirebaseAuthPackage(),
-            new RNFirebaseDatabasePackage(),
-            new VectorIconsPackage(), 
-            new RNGestureHandlerPackage());
+      return Arrays.<ReactPackage>asList(new MainReactPackage(), new SvgPackage(), new RNFirebasePackage(),
+          new RNFirebaseAuthPackage(), new RNFirebaseDatabasePackage(), new VectorIconsPackage(),
+          new RNGestureHandlerPackage());
     }
 
     @Override
--- app/config/LoginStackNavigator.js
+++ app/config/LoginStackNavigator.js
@@ -3,6 +3,11 @@ import LoginScreen from "../screens/LoginScreen";
 import RegistrationScreenStart from "../screens/RegistrationScreenStart";
 
 export default createStackNavigator({
-    LoginScreen,
+    LoginScreen: {
+        screen: LoginScreen,
+        navigationOptions: {
+            header: null
+        }
+    },
     RegistrationScreenStart
 });
