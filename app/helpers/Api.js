2d61c7a6d6fb402a9ce72afdbffcd7cd820c9f11
unknown <jelmer.haarman@xs4all.nl>
2019-01-07 14:09:52 +0100

    changed api conf

--- app/helpers/Api.js
+++ app/helpers/Api.js
@@ -2,7 +2,7 @@ import React from "react";
 import { NetInfo } from "react-native";
 let instance = null;
 class Api {
-  ip = "http:/145.37.145.36:8000"
+  ip = "http:/145.37.145.218:8000"
   url = this.ip + "/api/";
   mediaUrl = this.ip + "/media";
 
