e7dfe35cc82099c34fbd906b7966476fde679a78
Gaauwe <gromdroid@gmail.com>
2019-02-04 12:07:39 +0100

    API CHANGES

--- app/helpers/Api.js
+++ app/helpers/Api.js
@@ -4,7 +4,7 @@ import User from "./User";
 let instance = null;
 let userToken = null;
 class Api {
-  ip = "http://145.37.144.182:8000"
+  ip = "http://136.144.186.136";
   url = this.ip + "/api/";
   mediaUrl = this.ip + "/media";
 
